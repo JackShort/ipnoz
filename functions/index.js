f15ed22acffd4bd494d33f8eb2c75f9a562d6f39
Joe Karaki <j.karaki98@gmail.com>
2019-12-04 10:21:33 -0500

    Stripe even better

---
+++ functions/index.js
@@ -0,0 +1,29 @@
+const functions = require('firebase-functions');
+const stripe = require('stripe')('sk_test_eJl8URJUU8A9rbxaTrPAd5mg00a0dEOTo6');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.payWithStripe = functions.https.onRequest((request, response) => {
+    // Set your secret key: remember to change this to your live secret key in production
+    // See your keys here: https://dashboard.stripe.com/account/apikeys
+
+    // eslint-disable-next-line promise/catch-or-return
+    stripe.charges.create({
+        amount: request.body.amount,
+        currency: request.body.currency,
+        source: request.body.token,
+    // eslint-disable-next-line
+    }).then((charge) => {
+            // asynchronously called
+            response.send(charge);
+        })
+        .catch(err =>{
+            console.log(err);
+        });
+
+});
